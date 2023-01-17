01ca1728186a25cb92f0fb8b315793c8457f9363
Dean Galliano <113016756+Dean-Galliano@users.noreply.github.com>
2023-01-18 00:46:53 +0900

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,33 @@
+num=int(input("what's is the pass?"))
+
+def login (password):
+    set_pass=num
+    if set_pass==password:
+        return True
+
+        
+    else:
+        return False
+
+    
+
+for i in range(10):
+    for j in range(10):
+        for k in range(10):
+            a=login(int((str(i)+str(j)+str(k))))
+            if a==True:
+                print("you did it")
+                print(str(i)+str(j)+str(k))
+                break
+            else:
+                print("you noob")
+                continue
+        else:
+            continue
+        break
+    else:
+        continue
+    break
+
+
+
